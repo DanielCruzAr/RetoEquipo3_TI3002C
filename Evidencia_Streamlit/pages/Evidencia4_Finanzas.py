9534eaf91804fa4d72f63f56f037c4a22aa706d5
diegarza <a01705984@tec.mx>
2023-11-27 11:48:57 -0600

    Terminado esqueleto

--- Evidencia_Streamlit/pages/Evidencia4_Finanzas.py
+++ Evidencia_Streamlit/pages/Evidencia4_Finanzas.py
@@ -38,3 +38,95 @@ producto_input = st.sidebar.multiselect('Año',
                                          subset_data4.groupby('año').count().reset_index()['año'].tolist())
 if len(producto_input) > 0:
     subset_data3 = subset_data4[subset_data4['año'].isin(producto_input)]
+
+################## Finanzas ##################
+
+st.title('Finanzas')
+st.markdown('Estado general de las finanzas de Calor y Control')
+
+################## Primera parte ##################
+
+c1, c2 = st.columns(2)
+
+df_sorted = ventas.nlargest(10, 'ventas_cant')
+fig = go.Figure(data=[go.Pie(
+    labels=df_sorted['producto'],
+    values=df_sorted['ventas_cant'],
+    hoverinfo='label+percent',
+    hole=0.5,
+    marker=dict(colors=px.colors.sequential.Reds_r),
+    # title='Top 10 productos más vendidos',
+    textinfo='value',
+    textposition='inside',
+)])
+
+fig.update_layout(
+    font_family='Verdana',
+    # font_color='#90131C',
+    title={
+        'text': 'Top 10 productos más vendidos',
+        # 'x': 0.5,
+        # 'y': 0.9
+    },
+    font_size=14,
+    legend={'x': 1, 'y': 0.5}
+)
+
+c1.plotly_chart(fig)
+
+ventas['total_almacen'] = ventas['costo_prom'] * ventas['existencias']
+total_almacen_sum = ventas['total_almacen'].sum()
+
+formatted_sum = f"${total_almacen_sum/1000000:.2f} M"
+c2.error(f"{formatted_sum} \nTotal de producto en almacén")
+
+################## Segunda parte ##################
+
+productos = ventascompras['producto'].tolist()
+
+import plotly.express as px
+
+fig2 = px.scatter(ventascompras, x='cantidad', y='total_monto', color='producto',
+                 title='Gráfica Cantidad vs Importe', color_continuous_scale='Reds_r')
+
+fig2.update_layout(
+    xaxis_title='Cantidad',
+    yaxis_title='Importe',
+    legend_title='Producto'
+)
+
+st.plotly_chart(fig2)
+
+################## Tercera parte ##################
+
+################## Gráfica compras y ventas ##################
+
+ventascompras['fecha'] = pd.to_datetime(ventascompras['fecha'], dayfirst=True)
+ventascompras['año'] = ventascompras['fecha'].dt.year
+
+df1 = ventascompras.sort_values(by=['fecha'], ascending=[True])
+fig3 = px.line(df1, x='fecha', y='total_monto', color='movimiento',
+              title='Compras y ventas de productos con mayor importancia',
+              labels={'fecha': 'Fecha', 'total_monto': 'Monto en Pesos'},
+              hover_name='producto',
+              hover_data=['cantidad', 'total_monto'],
+              color_discrete_map={'venta': 'green', 'compra': '#90131C'},
+              markers=True,
+              )
+
+fig3.update_layout(
+    font_family='Verdana',
+    font_color='Black',
+    font_size=14,
+    title={
+        'text': 'Compras y ventas de productos con mayor importancia',
+        'x': 0.5,
+        'y': 0.9
+    },
+    legend={'x': 1, 'y': 0.5},
+    # plot_bgcolor='White',
+    xaxis=dict(gridcolor='#D9D9D9'),
+    yaxis=dict(gridcolor='#D9D9D9')
+)
+
+st.plotly_chart(fig3)
